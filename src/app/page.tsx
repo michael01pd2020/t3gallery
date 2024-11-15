6114b2f90773f502e9effa69d23cd1eef23f71a1
Wong, Michael D <Wong.Michael@mayo.edu>
2024-11-15 18:12:18 -0500

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { db } from "~/server/db";
+
 
 const mockUrls = [
   "https://utfs.io/f/cu6ozZ7vgAbCCTG5RYeGJg5IcmotAeRTlV6KHBNZY8s0p9kb",
@@ -10,10 +12,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap">
+        {posts.map((post) => (
+          <div key={post.id} className="w-1/2 p-4">
+            <p className="text-white">{post.name}</p>
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-1/2 p-4">
             <img src={image.url} alt="image" />
@@ -23,4 +33,4 @@ export default function HomePage() {
       <h1>Hello (gallery in progress)</h1>
     </main>
   );
-} 
+}
