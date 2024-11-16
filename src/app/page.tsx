039158d220db6e5d63f01b0a5bec869292f39a60
Wong, Michael D <Wong.Michael@mayo.edu>
2024-11-15 19:34:36 -0500

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
 import { db } from "~/server/db";
-
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
-    orderBy: (images, { desc }) => [desc(images.createdAt)],
+    orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
-    <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
